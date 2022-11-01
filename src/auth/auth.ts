51314a3f0ae2da613e4905f631a92ff045fa1325
NullSploit01 <harshaldharmik099@gmail.com>
2022-11-02 01:34:21 +0530

    Code Refactor

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -7,9 +7,12 @@ import { CurrentUserRouter } from "./routes/current-user";
 
 const router = express.Router();
 
-router.use("/auth", SigninRouter);
-router.use("/auth", SignupRouter);
-router.use("/auth", SignoutRouter);
-router.use("/auth", CurrentUserRouter);
+router.use(
+  "/auth",
+  SigninRouter,
+  SignupRouter,
+  SignoutRouter,
+  CurrentUserRouter
+);
 
 export { router as AuthRouter };
